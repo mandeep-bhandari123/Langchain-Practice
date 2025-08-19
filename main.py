fde806fa8a2818cfc9f6b9e76fa5449535f8fae3
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-08-19 17:26:03 +0545

    learned prompts

--- main.py
+++ main.py
@@ -1,60 +1,33 @@
-<<<<<<< HEAD
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_core.messages import HumanMessage , AIMessage , SystemMessage
+# load_dotenv()
+
+# llm = ChatGoogleGenerativeAI(
+#     model = "gemini-2.5-flash",
+# )
+# message = [
+#     SystemMessage(content="Solve the math problem"),
+#     HumanMessage(content='define set and give an eexample of what is a set and what is not a set')
+# ]
+
+# result = llm.invoke(message)
+# print(result.content)
+
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage , AIMessage , SystemMessage
+from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model = "gemini-2.5-flash",
+    model = "gemini-2.5-flash"
 )
-message = [
-    SystemMessage(content="Solve the math problem"),
-    HumanMessage(content='define set and give an eexample of what is a set and what is not a set')
-]
+template= [("system", " you are an expert on {topic}" ) , ("human" , "tell me the answwer of this {question}")]
 
-result = llm.invoke(message)
+prompt_template = ChatPromptTemplate.from_messages(template)
+
+prompt= prompt_template.invoke({"topic":"cats", "question":"Why do cat land on their feet always?"})
+print(prompt)
+result = llm.invoke(prompt)
 print(result.content)
-=======
-import os
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage, SystemMessage , AIMessage
-
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_google_genai import embeddings
-
-# Access the API key from the environment variable
-api_key = os.environ.get("GEMINI_API_KEY")
-
-
-
-model = GoogleGenerativeAI(model="gemini-2.0-flash", api_key=api_key)
-
-current_dir=os.path.dirname(os.path.abspath(__file__))
-file_path=os.path.join(current_dir,"books","odyssey.tst")
-persistent_directory=os.path.join(current_dir,'db','croma_db')
-
-if not os.path.exists(persistent_directory):
-    print("Directory doesnot exist")
-    
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(
-            f"the file {file_path} doesnot exist. please check the path."
-        )
-loader=TextLoader(file_path)
-documents = loader.load()
-    
-text_splitter = CharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
-docs=text_splitter.split_documents(documents)
-
-print("\n--- Document Chunk Information ---")   
-print(f"Number of document chunks :{len(docs)}")
-print(f"Sample chunk :\n{docs[0].page_content}")
-
-print("\n --- Creting Embeddings ---")
-
-embeddings = embeddings(
-    model=""
-)    
->>>>>>> ba9c2716490e26f178d6af5f93affe217eb58779
